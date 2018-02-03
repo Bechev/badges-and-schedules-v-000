e33c9e981cd9862f4013b21d891f18c8b9631f28
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-03 19:41:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,14 @@
 def badge_maker(name)
  return  "Hello, my name is #{name}."
 end
+
+
+def batch_badge_creator(names)
+  list_of_badges =[]
+  index = 0
+  names.each do |element|
+    list_of_badges[index]= badge_maker(element)
+    index += 1
+  end
+  return list_of_badges
+end
